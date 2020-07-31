8a7ddb43bc03ad50cf9525b9d35d4eea1d8c740b
MantaranSingh <meekmantaran@gmail.com>
2020-08-01 01:46:13 +0530

    Updates

---
+++ precache-manifest.b88159d769e82aac691b0e7e13bf66bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56dda382eddb2543fda27092e7b311d2",
+    "url": "/React_numGuessApp/index.html"
+  },
+  {
+    "revision": "fe9d3f446e583a91e355",
+    "url": "/React_numGuessApp/static/css/main.6e2e996a.chunk.css"
+  },
+  {
+    "revision": "7eeb0d3aa00c6d39b2b5",
+    "url": "/React_numGuessApp/static/js/2.9f4db709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_numGuessApp/static/js/2.9f4db709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe9d3f446e583a91e355",
+    "url": "/React_numGuessApp/static/js/main.1378b1f1.chunk.js"
+  },
+  {
+    "revision": "a015680ca83c22f1f40c",
+    "url": "/React_numGuessApp/static/js/runtime-main.5fef836b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React_numGuessApp/precache-manifest.b88159d769e82aac691b0e7e13bf66bc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_numGuessApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
